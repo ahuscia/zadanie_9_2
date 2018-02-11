aa7a76d767941215653262b99259f836f3a9e1c2
ahuscia <a.huscia@gmail.com>
2018-02-11 11:39:25 +0100

    Add index.html and script.js

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames); 
+
+var newName = 'Marian'
+
+if (allNames.indexOf(newName) === -1) {
+    new.Names.push(allNames);
+}
+
+consol.log (allNames);
+    
